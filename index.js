ad9d68da89bc521fe760d8e379b0b552c1a7eaa0
BoGhazi09 <faladasany@gmail.com>
2026-04-14 11:22:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const app = express();
 app.get("/", (req, res) => res.send("Bot is alive"));
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Web server running on port ${PORT}`);
-});
-
+app.listen(3000, () => console.log("Web server running"));
 // ====== CONFIG ======
 const TOKEN = process.env.TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
