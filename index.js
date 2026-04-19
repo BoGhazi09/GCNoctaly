7450052c9189d44a4c8fdaa5613fc3ca660ee7a7
BoGhazi09 <faladasany@gmail.com>
2026-04-19 12:05:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const {
   Events 
 } = require('discord.js');
 
-const config = require('./config.json');
-
 const OWNER_ROLE_ID = "1478554422303916185";
 
 const client = new Client({
@@ -26,7 +24,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
   if (interaction.isChatInputCommand()) {
     if (interaction.commandName === 'sendmessage') {
 
-      // 🔒 Role check
+      // Role check
       if (!interaction.member.roles.cache.has(OWNER_ROLE_ID)) {
         return interaction.reply({
           content: "You don't have permission to use this command.",
@@ -38,13 +36,13 @@ client.on(Events.InteractionCreate, async (interaction) => {
         .setCustomId('sendMessageModal')
         .setTitle('Send a Message');
 
-      const messageInput = new TextInputBuilder()
+      const input = new TextInputBuilder()
         .setCustomId('messageInput')
-        .setLabel('What message do you want to send?')
+        .setLabel('Enter your message')
         .setStyle(TextInputStyle.Paragraph)
         .setRequired(true);
 
-      const row = new ActionRowBuilder().addComponents(messageInput);
+      const row = new ActionRowBuilder().addComponents(input);
       modal.addComponents(row);
 
       await interaction.showModal(modal);
@@ -58,14 +56,14 @@ client.on(Events.InteractionCreate, async (interaction) => {
       const message = interaction.fields.getTextInputValue('messageInput');
 
       await interaction.reply({
-        content: 'Message sent!',
+        content: "Message sent!",
         ephemeral: true
       });
 
-      interaction.channel.send(message);
+      await interaction.channel.send(message);
     }
   }
 
 });
 
-client.login(config.token);
+client.login(process.env.TOKEN);
